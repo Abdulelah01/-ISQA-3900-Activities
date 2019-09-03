74f94f0e7744d574c8269cf9ec8c49cf8e510bc6
Abdulelah Almubarak <54547983+Abdulelah01@users.noreply.github.com>
2019-09-03 12:04:04 -0500

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<!--
+Name        : Abdulelah Almubarak
+Class       : 3900 
+Due         : Sep.3
+
+
+Name: Abdulelah Almubarak
+Email: aalmubarak@unomaha.edu
+
+
+-->
+
+
+<html lang="en">
+
+    <head>
+        <LINK REL=StyleSheet HREF="style.CSS" TYPE="text/css">
+        <title>About me</title>
+        <meta charset="utf-8"/>
+    </head>
+
+    <body>
+
+
+        <!-- all the page code goes between the body and /body tags -->
+        <div id="content" style= "width:650px">
+
+
+            <div>
+
+                <img src="images/Pic.jpg"  alt="a pic of my hero" style="width:300px; height:400px; float:left;" />
+
+                <p> This is my picture </p>
+
+
+
+                <h1> About Me</h1> 
+
+
+                My name is <span style="color:blue;"> Abdulelah Almubarak </span>
+                I'm from Saudi Arabia. My major
+                is Management Information System. I graduated from King fahad
+                University of petroleum with a bachelor degree in drafting. My life in the US is
+                pretty quiet. I spend most of my time between school and home. I enjoy watching shows and my favorate show is Last Man Standing. Its a family show. 
+                <br> 
+
+
+
+                <div id="educ" style= "width:650px;"> 
+                    <br><br>
+
+
+
+                    <h1> Education </h1>
+                    <p>
+
+
+                    I have a Bachelor degree in Drafting from King Fahd University  
+                    of Petroleum and Minerals. I enjoy drawing buildings that is                    
+                    the reason why I choose to be a draftman.Now i'm going to UNO 
+                    to get my second degree in<span style="color:blue;"> MIS</span>. I'm a senior this year.     
+                </div>  <!-- this closes the education section --> 
+
+                <div id= "rec" style= "width:650px;">
+
+                    <h1> Recreation </h1>
+                    <p>
+
+
+
+                    One of my hoppies is riding bikes. I rod my bike on many trails
+                    and my favorite is the Wabash trail located in Iowa. Every year
+                    Iowa held the biggest bike ride Nation wide which called 
+                    RAGBRAI, I have been on this ride twice in the last two
+                    years and this year I finsihed 500 miles.  
+                    . Another thing I like to do during my free time is outdoor Ice 
+                    skate at two of my favaiort places which are Cunnigham lake 
+                    and Manawa Lake. Unfortunatly Cunnigham lake is under constraction and it's frasturating not being able to enjoy the lake.                   
+                </div> 
+
+
+                <div id="quote" style= "width:650px;">       
+
+                    <h1> Quotes </h1>
+                    <p>
+
+                    <blockquote cite="Quotland.com">
+
+                        <b> A. </b> 
+                        <em> None of us will ever accomplish anything excellent or 
+                            commanding except when he listens to this whisper which is 
+                            heard by him alone.</em>
+                        by <b> Ralph Waldo Emerson </b> 
+                    </blockquote> 
+
+
+                    <blockquote cite="Quotationspage.com"   >
+
+                        <b> B. </b>
+                        <em> 
+                            Few people are capable of expressing with equanimity opinions
+                            wich differ from the prejudices of their social environment.
+                            Most people are even incable of forming such opinions.</em>
+                        by <b> Albert Einstein </b> 
+                    </blockquote>
+
+                    <blockquote   cite="Quoteationspage.com" >
+
+                        <b> C. </b>
+                        <i>
+                            Conversation should be pleasant without scurrility, witty 
+                            without affectation, free without indecency, learned without
+                            conceitedness, novel without falshood. </i>
+
+                        by <b> William Shakespeare </b> 
+                    </blockquote>  
+
+                </div><!-- this closes the Quotes section -->
+
+            </div> <!-- this code closes the div section we opened for 
+            "content" -->
+            <!-- this is the code that produces the Validator Badge for the
+            W3 Consortim -->
+            <!-- as you will notice it is in a separate div all of its own,
+            this is a design choice, as it is perfectly permissiable to
+            place it in the "content" div -->  
+
+
+
+
+
+            <div id="validator">
+                <p>
+                <a href="http://validator.w3.org/check?uri=referer">
+                    <img
+                    src="http://www.w3.org/html/logo/badge/html5-badge-h-css3.png"
+                    width="66" height="44" alt="HTML 5 Validator"
+                    title="HTML5 Validator">
+                </a>
+                &nbsp;
+                &nbsp;
+                <a  href="http://jigsaw.w3.org/css-validator/check/referer">
+                    <img style="border:0;width:88px;height:31px"
+                    src="http://jigsaw.w3.org/css-validator/images/vcss"
+                    alt="Valid CSS!" />
+                </a>
+
+                </p>
+            </div> 
+
+            <!-- our page content is finished, below this is only closing 
+            tag -->
+
+
+
+        </div> <!-- this ends the content div -->
+    </body>
+
+
+</html>   
+
+
+
+
+
+
+
+
+
+
+
+
